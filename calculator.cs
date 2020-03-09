083eb9e08ff6af5cf191eae2a5f9027cb7f671af
RyanSeiha <ryan.aryanatama@students.ac.id>
2020-03-09 17:00:07 +0700

    Menambahkan Fungsi lain

--- calculator.cs
+++ calculator.cs
@@ -2,9 +2,20 @@ class Calculator
 {
 	static void Main (string[] args)
 	{
-		
+		int a = 10;
+		int b = 6;
+
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 	static int penambahan(int a, int b)
+	{
+		return a + b ;
+	}
+	static int pengurangan(int a, int b)
 	{
 		return a + b ;
 	}
