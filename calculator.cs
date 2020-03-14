22a518e1e3dc9b374c5316a7cf68d5072b0a9f02
RyanSeiha <61868500+RyanSeiha@users.noreply.github.com>
2020-03-14 10:00:00 +0700

    Add files via upload
    Menambahkan fungsi flow control

--- calculator.cs
+++ calculator.cs
@@ -2,21 +2,78 @@ class Calculator
 {
 	static void Main (string[] args)
 	{
-		int a = 10;
-		int b = 6;
+        int a;
+        int b;
+        double c, d;
+        int pilih;
 
-		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
-		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+        Console.WriteLine("Pilih Menu Kalkulator");
+        Console.WriteLine("1. Penjumlahan");
+        Console.WriteLine("2. Pengurangan");
+        Console.WriteLine("3. Perkalian");
+        Console.WriteLine("4. Pembagian");
 
-		Console.WriteLine("\nTekan sembarang key untuk keluar");
-		Console.ReadKey();
-	}
-	static int penambahan(int a, int b)
-	{
-		return a + b ;
-	}
-	static int pengurangan(int a, int b)
-	{
-		return a + b ;
-	}
+        Console.WriteLine("Pilih Menu Kalulator (1/2/3/4)");
+        pilih = Convert.ToInt32(Console.ReadLine());
+
+        if (pilih == 1)
+        {
+            Console.Write("Masukan Angka 1 :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Masukan Angka 2 : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hasil : " + penambahan(a, b));
+        }
+        else if (pilih == 2)
+        {
+            Console.Write("Masukan Angka 1 :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Masukan Angka 2 : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hasil : " + pengurangan(a, b));
+        }
+        else if (pilih == 3)
+        {
+            Console.Write("Masukan Angka 1 :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Masukan Angka 2 : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hasil : " + perkalian(a, b));
+        }
+        else if (pilih == 4)
+        {
+            Console.Write("Masukan Angka 1 :");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Masukan Angka 2 : ");
+            d = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Hasil : " + pembagian(c, d));
+        }
+        else
+        {
+            Console.WriteLine("Kode Salah Bro!!!");
+        }
+
+        Console.WriteLine("\nTekan sembarang key untuk keluar");
+        Console.ReadKey();
+    }
+    static int penambahan(int a, int b)
+    {
+        return a + b;
+    }
+    static int pengurangan(int a, int b)
+    {
+        return a - b;
+    }
+    static int perkalian(int a, int b)
+    {
+        return a * b;
+    }
+    static double pembagian(double a, double b)
+    {
+        return a / b;
+    }
 }
